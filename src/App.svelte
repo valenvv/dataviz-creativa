f5aa682067422930c470df5a1552971d1da41258
valenvv <valentinavitetta5@gmail.com>
2024-04-26 21:11:27 -0300

    mejoras post pre entrega

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
   .domain(["Fantasía", "No Ficción", "Ninguno", "Ciencia Ficción", 
   "Romance", "Misterio"])
     .range([["#9F00D7","#CF78ED"], ["#E53619","#FF8975"],["#767676","#D4CEBA"], ["#004DB6","#5BA1FF"],
-     ["#FF586C","#F8C3C1"],  ["#00B554","#00FF77"]])
+     ["#FF586C","#FF8B87"],  ["#00B554","#00FF77"]])
     .unknown(["#FECE2E","#FFE695"])
 
   // Define una función de dominio personalizada para asignar "Otros" a cualquier género no especificado
@@ -134,7 +134,7 @@
     <p class="bajada">Explorando las preferencias literarias de la clase a través de datos</p>
   </div>
   <!-- Controles de filtro por género  -->
-  <!-- <div class="filtro">
+  <div class="filtro">
     <select bind:value={filtroGenero} on:change={(e) => setFilter(filtroGenero, e.target.value)}>
       <option value="">Todos los géneros</option>
       {#each colorGenero.domain() as genero}
@@ -143,8 +143,13 @@
     </select>
    
   </div>
-   -->
-  <!-- Controles de filtro por motivación
+  
+  <!-- <span class="boton-ref" style="background-image: url('public/images/fLectA.svg');"></span> -->
+  <a href="#abajo" class="boton-ref-link">
+    <img class="boton-ref" src="public\images\iconRefs.png" width="100px" height="100px" alt="">
+  </a>
+  
+  <!-- Controles de filtro por motivación -->
   <select bind:value={filtroMotivacion} on:change={(e) => setFilter(filtroMotivacion, e.target.value)}>
     <option value="">Todas las motivaciones</option>
     {#each Array.from(new Set(datos.map(d => d.Motivacion))) as motivacion}
@@ -152,13 +157,6 @@
     {/each}
   </select>
   
-     Controles de filtro por importancia 
-  <select bind:value={filtroImportancia}>
-    <option value="">Todas las importancias</option>
-    {#each Array.from(new Set(datos.map(d => d.Importancia))) as importancia}
-      <option value={importancia}>{importancia}</option>
-    {/each}
-  </select> -->
   <div class="container">
     {#each datosFiltrados as item}
       <div class="elemento">
@@ -168,12 +166,12 @@
                 <img class="mariposas"src="public/images/mariposas.svg" alt="SVG Mariposas" width="40" />
             </div>
           {/if}
-          <object  class="centro" data="{obtenerSVGCentro(item.Formato)}" type="image/svg+xml" width="40" height="40" aria-label="Centro SVG">
+          <object  class="centro" data="{obtenerSVGCentro(item.Formato)}" type="image/svg+xml" width="38" height="36" aria-label="Centro SVG">
             <img class="{item.Formato}"src="{obtenerSVGCentro(item.Formato)}" alt="SVG Centro"  />
           </object>
           {#if obtenerSVGFlor(item.Motivacion)}
             {#if item.Motivacion == "Aprendizaje"}
-              <svg class="flor"  height="120" viewBox="0 0 417 460" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg class="flor"  height="110" viewBox="0 0 417 460" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M141.16 180.557C142.247 183.238 138.934 174.319 133.705 166.801C53.1255 50.8829 140.704 -63.8957 192.972 41.6545C207.921 71.8428 234.89 305.408 141.16 180.557Z" fill="{cambiarColor(item.Genero)[1]}"/>
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M208.379 191.222C206.861 192.898 208.034 188.385 208.049 188.299C220.327 119.735 330.363 -62.4685 407.905 46.1042C474.735 139.667 146.457 307.37 208.049 188.299" fill="{cambiarColor(item.Genero)[1]}"/>
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M226.035 219.733C214.591 223.737 279.778 213.32 294.493 216.931C417.372 247.098 469.795 402.16 341.789 380.376C327.003 377.86 132.939 216.708 262.393 215.571" fill="{cambiarColor(item.Genero)[1]}"/>
@@ -183,14 +181,14 @@
                 </svg>
             {/if}
             {#if item.Motivacion == "Entretenimiento"}
-              <svg class="flor"  height="120" viewBox="0 0 417 460" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg class="flor"  height="110" viewBox="0 0 417 460" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M92.2464 120.469C78.3641 70.9254 106.421 19.3339 155.552 4.05512C178.718 -3.15174 203.819 -0.673542 225.131 10.9172C246.447 22.5048 262.168 42.226 268.711 65.5894C269.884 69.7757 270.58 72.2744 270.58 72.2744C270.58 72.2744 270.58 72.2744 270.585 72.2744C290.177 57.8575 314.865 52.1974 338.779 56.6382C362.694 61.0838 383.7 75.2334 396.804 95.728C398.246 97.9832 399.692 100.242 401.133 102.497C429.59 147.003 418.635 205.956 376.088 237.268C417.594 271.838 425.51 332.531 394.255 376.595C394.084 376.832 393.918 377.071 393.749 377.31C379.468 397.441 357.436 410.7 332.962 413.89C308.489 417.081 283.792 409.914 264.827 394.118C264.827 394.118 264.297 395.763 263.378 398.616C255.92 421.721 239.257 440.724 217.32 451.135C195.39 461.545 170.13 462.44 147.515 453.609C98.5677 434.495 72.9712 380.569 89.1087 330.561C90.0276 327.709 90.559 326.063 90.559 326.063C39.3518 324.14 -0.900367 281.593 0.0153173 230.357C0.100718 225.556 0.186119 220.737 0.274682 215.936C0.708012 191.639 10.8976 168.538 28.5502 151.838C46.2012 135.137 69.8335 126.243 94.1173 127.154C94.1173 127.154 93.4167 124.655 92.2464 120.469Z" fill="{cambiarColor(item.Genero)[1]}" />
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M179.649 178.454C205.426 156.74 246.102 164.453 270.435 195.664C294.763 226.875 293.593 269.842 267.816 291.556C242.042 313.269 201.365 305.556 177.033 274.345C152.705 243.134 153.876 200.167 179.649 178.454Z" fill="{cambiarColor(item.Genero)[0]}"/>
               </svg>
             {/if}
             {#if item.Motivacion == "Ambas"}
               <!-- Flor - AP Y ENT -->
-              <svg class="flor"  height="120" viewBox="0 0 529 460" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg class="flor"  height="110" viewBox="0 0 529 460" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M295.266 203.926C291.721 204.31 303.46 203.319 313.793 200.191C473.098 151.999 600.754 261.818 459.491 285.835C419.088 292.704 122.438 262.365 295.266 203.926Z" fill="{cambiarColor(item.Genero)[0]}"/>
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M227.748 181.932C226.291 183.469 227.418 179.332 227.432 179.254C239.221 116.412 344.878 -50.5833 419.334 48.9271C483.504 134.68 168.291 288.386 227.432 179.254" fill="{cambiarColor(item.Genero)[0]}"/>
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M249.095 274.598C239.644 273.783 289.051 287.719 298.074 295.338C373.42 358.968 354.783 492.777 272.849 433.725C263.386 426.904 184.929 241.265 276.063 283.607" fill="{cambiarColor(item.Genero)[0]}"/>
@@ -203,7 +201,7 @@
             {/if}
             {#if item.Motivacion == "Ninguno"}
               <!-- Flor - Nada -->
-              <svg class="flor" height="120" viewBox="0 0 404 460" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg class="flor" height="110" viewBox="0 0 404 460" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M23.9795 65.0812C25.3517 54.8245 0.226326 141.616 0.00789924 171.221C-0.648964 259.707 39.7252 341.32 96.915 373.5C188.607 425.092 228.08 407.464 275.769 405.457C332.164 403.083 370.905 346.223 392.53 259.406C411.762 182.185 414.03 36.9348 341.249 27.2746C295.288 21.1708 272.041 76.0011 263.901 130.062C264.221 132.377 263.553 132.368 263.901 130.062C255.909 72.8814 246.792 -8.31524 177.885 0.689062C150.17 4.31419 152.443 84.9683 145.186 114.631C147.188 128.178 141.957 127.821 145.186 114.631C125.664 -17.2824 20.9041 7.34845 5.49706 136.835" fill="{cambiarColor(item.Genero)[1]}"/>
                 <mask id="mask0_50_52" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="54" y="362" width="297" height="98">
                 <path d="M54 362.98H350.359V460H54V362.98Z" fill="white"/>
@@ -221,21 +219,21 @@
           {/if}
         
         <div class="tallo">
-          <object class="tallo" data="{obtenerSVG(item.Horas)}" type="image/svg+xml" height="100" aria-label="Tallo SVG">
+          <object class="tallo" data="{obtenerSVG(item.Horas)}" type="image/svg+xml" height="90" aria-label="Tallo SVG">
             <img src="public/images/default.svg" alt="SVG Tallo" />
           </object>
         </div>
         {#if item.LibroFav != null }
           <p class="libro">{item.LibroFav}</p>
         {:else}
-          <p></p>
+          <p>:(</p>
         {/if}
       </div>
       
     {/each}
   </div>
   <div class="r-gradient"></div>
-  <h2>Referencias</h2>
+  <h2 id="abajo">Referencias</h2>
   <div class="referencias">
     <div class="r-genero">
       <p class="r-titlulo">Género de lectura de preferencia:</p>
@@ -285,11 +283,48 @@
       </ul>
     </div>
   </div>
+  <footer class="footer" width="100%">
+    <div class="waves">
+      <div class="wave" id="wave1"></div>
+      <div class="wave" id="wave2"></div>
+      <div class="wave" id="wave3"></div>
+      <div class="wave" id="wave4"></div>
+    </div>
+    <ul class="social-icon">
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-facebook"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-twitter"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-instagram"></ion-icon>
+        </a></li>
+    </ul>
+    <ul class="menu">
+      <li class="menu__item"><a class="menu__link" href="#">Home</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">About</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">Services</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">Team</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">Contact</a></li>
+
+    </ul>
+    <p>&copy;2021 Nadine Coelho | All Rights Reserved</p>
+  </footer>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </main>
 
 
 <style>
   /* Estilos */
+
+  .headline{
+    font-family: "DM Sans";
+  }
   .container {
     display: flex;
     flex-wrap: wrap;
@@ -297,15 +332,16 @@
     gap: 20px;
     padding: 20px;
     max-width: 1200px;
-    margin: 0 auto;
     position: relative;
+    margin-right: 15px;
+    margin-left: 15px;
   }
 
   .elemento {
     flex-grow: 1;
     background-color: #ffe0c785;
-    width: 200px;
-    max-width: 200px;
+    width: 250px;
+    max-width: 250px;
     position: relative;
     display: flex;
     flex-direction: column;
@@ -313,17 +349,22 @@
     padding: 20px;
     border-radius: 10px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    margin-bottom: 20px;
+    margin-right: 15px;
+    margin-left: 15px;
+    margin-bottom: 30px;
     transition: all 0.4s ease;
   }
 
   .elemento:hover {
     transform: scale(1.04);
+    /* box-shadow: 0 0 100px #cc25577e; */
   }
 
   .nombre {
-    margin-top: 10px;
-    font-weight: bold;
+    margin-top: 20px;
+    margin-bottom: 40px;
+    font-weight:bolder;
+    font-size: larger;
   }
   .header{
     text-align: center;
@@ -331,7 +372,7 @@
 
   .centro{
     position: absolute;
-    transform: translate(7%, 225%);
+    transform: translate(6%, 352%);
   }
 
   .Entretenimiento{
@@ -343,8 +384,8 @@
 
   .mariposas{
     position: absolute;
-    right: 70%;
-    top: 17%;
+    right: 65%;
+    top: 26%;
   }
 
   .libro{
@@ -360,9 +401,11 @@
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-    gap: 20px;
-    padding: 2px;
-    
+    gap: 30px;
+    padding: 20px;
+    margin-right: 15px;
+    margin-left: 15px;
+    margin-bottom: 200px;
   }
 
   .r-titlulo{
@@ -381,8 +424,10 @@
     margin-top: 20px;
     margin-left: 5px;
     align-items: center;
-    width: 120px;
+    width: 220px;
+    max-width: 220px;
     position: relative;
+    padding:20px;
 
   }
 
@@ -458,6 +503,22 @@
     margin-right: 10px;
   }
 
+  /* Boton estático para ir a las referencias */
+  .boton-ref{
+    display: inline-block;
+    padding: 20px;
+    
+    cursor:pointer;
+    position:fixed;
+    bottom: 0;
+    right: 0;
+    z-index: 2;
+  }
+  
+  .boton-ref-link:target .boton-ref {
+    display: none;
+  }
+
  /*Filtros*/
 
   .filtro {
@@ -503,5 +564,131 @@
     position: relative;
   }
   
+
+  /* FOOTER! */
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap");
+
+
+  .footer {
+    position: relative;
+    width: 100%;
+    height: 300px;
+    background: #CC2557;
+    min-width:100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .social-icon,
+  .menu {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10px 0;
+    flex-wrap: wrap;
+  }
+
+  .social-icon__item,
+  .menu__item {
+    list-style: none;
+  }
+
+  .social-icon__link {
+    font-size: 2rem;
+    color: #fff;
+    margin: 0 10px;
+    display: inline-block;
+    transition: 0.5s;
+  }
+  .social-icon__link:hover {
+    transform: translateY(-10px);
+  }
+
+  .menu__link {
+    font-size: 1.2rem;
+    color: #fff;
+    margin: 0 10px;
+    display: inline-block;
+    transition: 0.5s;
+    text-decoration: none;
+    opacity: 0.75;
+    font-weight: 300;
+  }
+
+  .menu__link:hover {
+    opacity: 1;
+  }
+
+  .footer p {
+    color: #fff;
+    margin: 15px 0 10px 0;
+    font-size: 1rem;
+    font-weight: 300
+    
+;
+  }
+
+  /* en figma */
+
+  .wave { 
+    position: absolute;
+    top: -100px;
+    left: 0;
+    width: 100%;
+    height: 100px;
+    background-image: url("public/images/wave.png");
+    background-color: CC2557;
+    background-size: 1000px 100px;
+  }
+
+  .wave#wave1 {
+    z-index: 1000;
+    opacity: 1;
+    bottom: -1;
+    animation: animateWaves 8s linear infinite;
+  }
+
+  .wave#wave2 {
+    z-index: 999;
+    opacity: 0.5;
+    bottom: 10px;
+    animation: animate 8s linear infinite !important;
+  }
+
+  .wave#wave3 {
+    z-index: 1000;
+    opacity: 0.2;
+    bottom: 15px;
+    animation: animateWaves 7s linear infinite;
+  }
+
+  .wave#wave4 {
+    z-index: 999;
+    opacity: 0.7;
+    bottom: 20px;
+    animation: animate 7s linear infinite;
+  }
+
+  @keyframes animateWaves {
+    0% {
+      background-position-x: 1000px;
+    }
+    100% {
+      background-positon-x: 0px;
+    }
+  }
+
+  @keyframes animate {
+    0% {
+      background-position-x: -1000px;
+    }
+    100% {
+      background-positon-x: 0px;
+    }
+  }
+ 
 
 </style>
